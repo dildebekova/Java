272d62577d58f38e3b4a0de3b91811711e2b6ccd
nazima <dildebekvass@gmail.com>
2026-02-10 22:40:35 +0600

    Add Book class

---
+++ src/Book.java
@@ -0,0 +1,57 @@
+public class Book {
+
+    // TODO: make fields private
+    private String title;
+    private String author;
+    private int year;
+    private boolean isAvailable;
+
+    // TODO: Implement parameterized constructor
+    public Book(String title, String author, int year) {
+        // implement
+    }
+
+    // TODO: Implement copy constructor
+    public Book(Book other) {
+        // implement
+    }
+
+    // TODO: Implement getters
+    public String getTitle() {
+        return null;
+    }
+
+    public String getAuthor() {
+        return null;
+    }
+
+    public int getYear() {
+        return 0;
+    }
+
+    public boolean isAvailable() {
+        return false;
+    }
+
+    // TODO: Implement borrow logic
+    public void borrowBook() {
+        // implement
+    }
+
+    // TODO: Implement return logic
+    public void returnBook() {
+        // implement
+    }
+
+    // TODO: Override toString()
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    // TODO: Override equals()
+    @Override
+    public boolean equals(Object obj) {
+        return false;
+    }
+}
