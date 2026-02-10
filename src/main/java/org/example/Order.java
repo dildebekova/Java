cad92cfccb48c4f56dc18ecde7bcdc7b6a8a7ad7
nazima <dildebekvass@gmail.com>
2026-02-10 23:02:59 +0600

    .

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,4 +1,20 @@
 package org.example;
 
 public class Order {
+    String name;
+    double price;
+    int count;
+
+    public Order(String name, double price, int count) {
+        this.name = name;
+        this.price = price;
+        this.count = count;
+    }
+}
+
+class OrderMain{
+    public static void main(String[] args){
+
+       
+    }
 }
